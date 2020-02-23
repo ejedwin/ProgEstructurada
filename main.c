b0a5f855c213cbe71470bd9930c6f5e7f382a89f
Edwin Diaz <ejedwin.co@gmail.com>
2020-02-23 12:52:22 -0500

    variables

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int enteroB;
+    float flotanteB;
+    char letraB;
+
+    printf("Ingrese un dato ");
+    scanf("%i", & enteroB);
+    printf("su dato es %i", enteroB);
+    return 0;
+}
